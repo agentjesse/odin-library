a30ede16fb653c9ff453b5d29faa4e95904d4cb4
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-28 23:39:35 -0500

    Added logic for showing new books

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const submitBookBtn= document.querySelector('#submitBookBtn');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const pagesInput = document.querySelector('#pages');
-const readInput = document.querySelector('#readState');
+const readInput = document.querySelector('#read');
 //library that stores book objects will be an array
 const myLibrary = [
   {
@@ -39,62 +39,49 @@ const myLibrary = [
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
+  this.pages = pages ? pages : 'N/A';
   this.read = read;
 }
 
-//create a book, add to library
-function addBookToLibrary() {
-  //should get user input, but using defaults for now
-  const title = 'The Three Not So Little Bears5', author = 'Jerry Smith', pages = 500, read = false;
-  //store the book
+//create a book, add to library array
+function addBookToLibrary(title, author, pages, read) {
   myLibrary.push( new Book(title, author, pages, read) )
 }
 
-//display library on page
-myLibrary.forEach( (item,index,arr)=> {
-  //make html elements
-  const bookElem = document.createElement('article');
-  const h3 = document.createElement('h3');
-  const h4 = document.createElement('h4');
-  const pagesPara = document.createElement('p');
-  const readPara = document.createElement('p');
-  //add html elements data
-  h3.textContent = item.title;
-  h4.textContent = item.author;
-  pagesPara.textContent = `Pages: ${item.pages}`;
-  readPara.textContent = `Read?: ${item.read}`;
-  bookElem.classList.add('book'); //styling class
-  bookElem.setAttribute('data-arr-index',`${index}`) //save index of book in library array, might need it
-  //append to parents
-  bookElem.append(h3, h4, pagesPara, readPara);
-  booksGrid.append(bookElem);
-});
+//build and display library on page
+const updateBooksGrid = ()=> {
+  booksGrid.textContent = ''; //clear books before adding
+  myLibrary.forEach( (item,index)=> {
+    //make html elements
+    const bookElem = document.createElement('article');
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    const pagesPara = document.createElement('p');
+    const readPara = document.createElement('p');
+    //add html elements data
+    h3.textContent = item.title;
+    h4.textContent = item.author;
+    pagesPara.textContent = `Pages: ${item.pages}`;
+    readPara.textContent = `Read?: ${item.read}`;
+    bookElem.classList.add('book'); //styling class
+    bookElem.setAttribute('data-arr-index',`${index}`) //save index of book in library array, might need it
+    //append to parents
+    bookElem.append(h3, h4, pagesPara, readPara);
+    booksGrid.append(bookElem);
+  });
+}
+updateBooksGrid(); //first run to populate page
 
 //event listeners
 //open modal without bg interactivity, also makes ::backdrop pseudo-element
-newBookBtn.addEventListener('click', ()=> newBookModal.showModal() )
+newBookBtn.addEventListener('click', ()=> newBookModal.showModal() );
 //close and open modal from buttons within a form with method=dialog
 closeModalBtn.addEventListener('click', ()=> newBookModal.close() );
-submitBookBtn.addEventListener('click', e =>{
-  //check at least title and author inputs filled, make object
-  if ( titleInput.checkValidity() && authorInput.checkValidity() ) {
-    const bookObj = {
-      "title": titleInput.value,
-      "author": authorInput.value,
-      "pages": pagesInput.value,
-      "read": readInput.checked,
-    }
-    //use data for new book, then close
-    
-    newBookModal.close();
+submitBookBtn.addEventListener('click', ()=> {
+  if ( titleInput.checkValidity() && authorInput.checkValidity() ) { //check at least title and author inputs filled
+    //use data for new book with the odin required addBookToLibrary fn
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
+    updateBooksGrid();
+    newBookModal.close(); //also sends a close event, if needed
   }
-  
 });
-
-
-//test book adding to library arr
-/*
-addBookToLibrary();
-console.log(`Library array: `, myLibrary);
-*/
